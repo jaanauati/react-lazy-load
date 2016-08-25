d3506524459b30587b4f6b98b41bd38409e0f415
Jonatan Alexis Anauati <barakawins@gmail.com>
2016-08-25 17:53:13 -0300

    catch exceptions, syntax sugar

--- src/utils/mutationsObserver.js
+++ src/utils/mutationsObserver.js
@@ -10,20 +10,24 @@ const _observerConfig = { childList: true, subtree: true };
  */
 if (typeof MutationObserver !== 'undefined') {
   _observer = new MutationObserver(function observerCallback() {
-    _callbacks.forEach((cb) => cb());
+    _callbacks.forEach((cb) => {
+      try {
+        cb();
+      } catch (ex) { } // eslint-disable-line no-empty
+    });
   });
   _observer.observe(document.body, _observerConfig);
 }
 
-export function registerCallback( cb ) {
-  if (_callbacks.indexOf(cb) !== -1 ) {
+export function registerCallback(cb) {
+  if (_callbacks.indexOf(cb) !== -1) {
     _callbacks.push(cb);
   }
 }
 
 export function unregisterCallback(cb) {
   const idx = _callbacks.indexOf(cb);
-  if ( idx !== -1 ) {
+  if (idx !== -1) {
     _callbacks.splice(idx, 1);
   }
 }
